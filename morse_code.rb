37f0131d110cb98a5de53e084813dbdb5620c408
Pablo <ezekielgonzalezr@gmail.com>
2023-07-19 10:23:49 -0600

    Create decode word method

--- morse_code.rb
+++ morse_code.rb
@@ -1,5 +1,5 @@
-def morse_char
-  {
+def decode_char (str)
+  alph = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
     '.' => 'E', '..-.' => 'F', '....' => 'H', '..' => 'I',
     '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M',
@@ -7,4 +7,21 @@ def morse_char
     '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W',
     '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
   }
+return alph[str]
+end
+
+def decode_word (str)
+  word = ''
+  char = ''
+  str.length.times do |i|
+    if str[i] == ' '
+      word = "#{word}#{decode_char(char)}"
+      char = ''
+    elsif i == str.length - 1
+      char = "#{char}#{str[i]}"
+      word = "#{word}#{decode_char(char)}"
+    else  char = "#{char}#{str[i]}"
+    end
+  end
+return word
 end
